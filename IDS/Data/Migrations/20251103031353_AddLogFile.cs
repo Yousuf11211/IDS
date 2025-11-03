cacdb816a2b05db9633046dbe9159271b4ad1d3d
Syed Yousuf Uddin <yousuffuddin2003@gmail.com>
2025-11-02 21:18:14 -0600

    add logfile table

---
+++ IDS/Data/Migrations/20251103031353_AddLogFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IDS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLogFile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LogFiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Level = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: false),
+                    Exception = table.Column<string>(type: "nvarchar(4000)", maxLength: 4000, nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogFiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogFiles_Timestamp",
+                table: "LogFiles",
+                column: "Timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogFiles");
+        }
+    }
+}
--- IDS/Pages/Dashboard.cshtml.cs
+++ IDS/Pages/Dashboard.cshtml.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IDS.Data;
+using IDS.Data.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IDS.Pages
@@ -7,8 +11,28 @@ namespace IDS.Pages
     [Authorize]
     public class DashboardModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public DashboardModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var log = new LogFile
+            {
+                Timestamp = DateTime.UtcNow,
+                Level = "Information",
+                Message = "Dashboard page visited",
+                Exception = null,
+                UserId = userId
+            };
+
+            _db.LogFiles.Add(log);
+            await _db.SaveChangesAsync();
         }
     }
 }
