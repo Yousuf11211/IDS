e1ec9b7622f3685af8d43061826bd1dbcf50713f
Yousuf11211 <yousuffuddin2003@gmail.com>
2026-09-24 10:01:27 -0500

    added testing mode

--- IDS/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ IDS/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -1,4 +1,5 @@
 using IDS.Data.Models;
+using IDS.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,15 +11,18 @@ namespace IDS.Areas.Identity.Pages.Account.Manage;
 public class TwoFactorAuthenticationModel : PageModel
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SecurityPolicyService _policies;
 
-    public TwoFactorAuthenticationModel(UserManager<ApplicationUser> userManager)
+    public TwoFactorAuthenticationModel(UserManager<ApplicationUser> userManager, SecurityPolicyService policies)
     {
         _userManager = userManager;
+        _policies = policies;
     }
 
     public bool HasAuthenticator { get; private set; }
     public int RecoveryCodesLeft { get; private set; }
     public bool Is2faEnabled { get; private set; }
+    public bool TestingBypass { get; private set; }
 
     [TempData]
     public string? StatusMessage { get; set; }
@@ -32,6 +36,7 @@ public class TwoFactorAuthenticationModel : PageModel
         }
 
         HasAuthenticator = await _userManager.GetAuthenticatorKeyAsync(user) != null;
+        TestingBypass = await _policies.CanBypassTwoFactorAsync(user);
         Is2faEnabled = await _userManager.GetTwoFactorEnabledAsync(user);
         RecoveryCodesLeft = await _userManager.CountRecoveryCodesAsync(user);
         return Page();
